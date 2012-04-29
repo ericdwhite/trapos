16d9a2acd45b857f189254614e57dca850bbfd26
Eric D. White <eric@ericwhite.ca>
2012-04-29 20:28:26 +0100

    Clean up some comments.

--- src/main/java/whitewerx/com/trapos/model/PositionFactory.java
+++ src/main/java/whitewerx/com/trapos/model/PositionFactory.java
@@ -1,30 +1,30 @@
 package whitewerx.com.trapos.model;
 
 /**
- * Creates positions so that they
- * can be easily tested.
+ * Creates positions so that they can be easily tested.
  * 
- *  @author ewhite
+ * @author ewhite
  */
 public class PositionFactory {
-    
+
     /**
-     * TODO: note this assumes the PNL currency is always the quote currency
-     *       in a real system this would not be correct.
-     *       
-     *       For example if the PNL currency is USD, and the position
-     *       is USDCAD then this would be incorrect.
-     *       
-     * @param currencyPair for the flat position.
+     * Note this assumes the PNL currency is always the quote currency in a real
+     * system this would not be correct.
+     * 
+     * For example if the PNL currency is USD, and the position is USDCAD then
+     * this would be incorrect.
+     * 
+     * @param currencyPair
+     *            for the flat position.
      * @return
      */
     public Position createFlatPositionFor(CurrencyPair currencyPair) {
         Amount ccy1Amount = new Amount(0, currencyPair.getBase());
         Amount ccy2Amount = new Amount(0, currencyPair.getQuote());
-        Amount ccy1EqvAmount = ccy2Amount; 
-        Amount ccy2EqvAmount = ccy2Amount; 
+        Amount ccy1EqvAmount = ccy2Amount;
+        Amount ccy2EqvAmount = ccy2Amount;
 
         return new Position(ccy1Amount, ccy2Amount, ccy1EqvAmount, ccy2EqvAmount, currencyPair, currencyPair.getQuote());
     }
-    
+
 }
